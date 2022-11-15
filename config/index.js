3e5046f275c21f7f02ac6907c10856f3ccb9349c
joeli <joelideveloper@gmail.com>
2022-11-15 17:12:27 +0000

    fix linting error

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
-    announceInterval: 5000,
-    protocol: 'http://'
-};
+  announceInterval: 5000,
+  protocol: 'http://'
+}
 
-export default config;
+export default config
--- util.js
+++ util.js
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+import crypto from 'crypto'
 
 // generate unique id as 32-chars string
 export const generateUniqId = () => crypto.randomBytes(16).toString('hex')
